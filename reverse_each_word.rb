a893d841a91e5d27a4488f88a71e4bbfdfea68d6
Ekene N <ekenayy@gmail.com>
2020-10-12 17:35:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,11 @@
 
 def reverse_each_word(sentence)
 sentence = sentence.split(" ")
-new_sentence = " "
+new_sentence = [ ]
 
 sentence.each do |element|
 element.reverse
-new_sentence = element.reverse
+new_sentence << element.reverse
 end
 
 new_sentence
