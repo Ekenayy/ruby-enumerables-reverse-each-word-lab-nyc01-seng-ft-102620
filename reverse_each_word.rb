3c86470f56f69f862049a85d07eae05716613175
Ekene N <ekenayy@gmail.com>
2020-10-12 17:40:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,21 @@
 #Turn the string into an array
 #
 
+#def reverse_each_word(sentence)
+#sentence = sentence.split(" ")
+#new_sentence = [ ]
+
+#sentence.each do |element|
+#element.reverse
+#new_sentence << element.reverse
+#end
+
+#new_sentence.join(" ")
+
+#end
+
 def reverse_each_word(sentence)
 sentence = sentence.split(" ")
-new_sentence = [ ]
 
-sentence.each do |element|
+sentence.collect do |element|
 element.reverse
-new_sentence << element.reverse
-end
-
-new_sentence.join(" ")
-
-end
